d4506a8b1918b9dc5819f9e1997c1e454a3bf424
Jared Michael Wolff <104460391+jaredmwolff@users.noreply.github.com>
2024-03-23 15:07:37 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿//--------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Data;
+using System.Xml.Serialization;
+
+//--------------------------------------------------------------------------------------------------------------------
+
+namespace Adventure
+{
+    public static class Game
+    {
+        //The default name, just in case
+        static string CharacterName = "John Smith";
+
+        //Here, the user inputs a name for the player
+        static void NameCharacter()
+        {
+            Console.WriteLine("What is your character's name?");
+            Console.Write("> ");
+            CharacterName = Console.ReadLine();
+
+            Console.WriteLine("Excellent! The name of your character is now " + CharacterName);
+            Console.ReadKey();
+        }
+
+        //This is where the ALL of the choices are
+        static void Choices()
+        {
+            Console.Clear();
+            Console.WriteLine("The player, " + CharacterName + ", wakes up in a well-lit cave with no prior memory of how they got there.");
+            Console.WriteLine("To your LEFT, you see a passageway, which could potentially lead you outside.");
+            Console.WriteLine("To your RIGHT, you see a wooden box. Perhaps there is something useful in there?");
+            Console.WriteLine();
+            Console.WriteLine("Decision: Where will the player go?");
+            Console.WriteLine("A) Left");
+            Console.WriteLine("B) Right");
+            Console.WriteLine();
+
+            Console.Write("> ");
+            string decision1 = Console.ReadLine();
+
+            if (decision1 == "A")
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("As " + CharacterName + " walks down this passageway, you notice a bright light down the end of the tunnel.");
+                Console.WriteLine();
+                Console.WriteLine("Decision: What will the player do?");
+                Console.WriteLine("A) Walk Towards the Light");
+                Console.WriteLine("B) Go Back to the Room You Woke Up In");
+                Console.WriteLine();
+
+                Console.Write("> ");
+                string decision2 = Console.ReadLine();
+
+                    if (decision2 == "A")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("As you walk towards the light, you quickly find yourself outside of the cave.");
+                        Console.WriteLine("Congratulations! You have escaped the cave!");
+                    }
+
+                    else if (decision2 == "B")
+                    {
+                        Console.Clear();
+                        Choices();
+                    }
+
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Invalid choice. Please answer with one of the following options: A or B");
+                    }
+            }
+
+            else if (decision1 == "B")
+            {
+                Console.Clear();
+                Console.WriteLine("As you approach the wooden box, you hear an odd, unfamiliar noise emanating from inside.");
+                Console.WriteLine();
+                Console.WriteLine("Decision: What will the player do?");
+                Console.WriteLine("A) Open the Box");
+                Console.WriteLine("B) Don't Open the Box");
+                Console.WriteLine();
+
+                Console.Write("> ");
+                string decision3 = Console.ReadLine();
+
+                if (decision3 == "A")
+                {
+                    Console.Clear();
+                    Console.WriteLine("As you open the box, you see a mysterious creature held within.");
+                    Console.WriteLine("Unfortunately for you, the creature is not pleased that you woke it up.");
+                    Console.WriteLine("The creature screeches in a manner you have never heard.");
+                    Console.WriteLine("Your consciousness quickly fades, until you pass out...");
+                    Console.WriteLine(CharacterName + " was never heard from again...");
+                }
+
+                else if (decision3 == "B")
+                {
+                    Console.Clear();
+                    Choices();
+                }
+
+                else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Invalid choice. Please answer with one of the following options: A or B");
+                    }
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid choice. Please answer with one of the following options: A or B");
+            }
+    }
+
+
+//--------------------------------------------------------------------------------------------------------------------
+
+
+    //The class that starts the game
+    //Additionally, it holds the properties to start the following: NameCharacter() & Choices()
+    public static void StartGame()
+    {
+        Console.Clear();
+        Console.WriteLine();
+        Console.WriteLine("Sprint Project: C# Coding Language");
+        Console.WriteLine();
+        Console.WriteLine("Welcome to the Text Adventure Game!");
+        Console.WriteLine();
+
+        NameCharacter();
+        Console.WriteLine();
+
+        Choices();
+        }
+    }
+
+
+//--------------------------------------------------------------------------------------------------------------------
+
+
+    //Runs the class that starts the game
+    class Program
+    {
+        static void Main()
+        {
+            Game.StartGame();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# Overview
+
+The purpose of this code is to further my understanding of the C# Coding Language. Setting up this language was a pain, but I managed to get it working somehow.
+
+Using C#, I crafted a simple Text-Based Adventure Game. It was simple enough so that I could familiarize myself with how the coding language worked.
+
+Again, the purpose of this program was to further my understanding of the C# Coding Language.
+
+[Software Demo Video](https://www.youtube.com/watch?v=u-lpZuzv7A8)
+
+
+
+# Development Environment
+
+Tools: Visual Studio Code
+
+Programming Language: C#
+
+
+
+# Useful Websites
+
+- [Geeks for Geeks Tutorial](https://www.geeksforgeeks.org/c-sharp-tutorial/)
+- [Programming is Fun Tutorial](https://programmingisfun.com/learn/c-sharp-adventure-game/)
+- [Miscellaneous Example Code that Helped](https://github.com/ShaunHalverson/TextBasedAdventureC-/blob/main/main.cs)
+
+
+
+# Future Work
+
+- I had totally forgotten to make the choices revolve around LOOP statements. That's the first thing to fix.
+- Adding in more routes would be pretty fun to do.
+- Instead of inputting just A or B for a response, I could make it so the user can input other options as well (for example, LEFT or RIGHT)
